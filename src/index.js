355639d4e37ba37a844db8d78c877d88a4a6f4ad
saikrishnamohan053 <saikrishnamohan.k@gmail.com>
2025-05-13 12:03:00 -0500

    modified index.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,12 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
